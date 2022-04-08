959114d14088f9d9e2f7c09aebaf792331e329d9
unknown <rubychandra45@gmail.com>
2022-04-08 22:31:48 +0700

    commit

--- word_cloud.py
+++ word_cloud.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 tf_vector = TfidfVectorizer()
 
 def cloudMaker(x, names):
-	cloud = WordCloud(background_color = "white", max_words=35, width=800, height=400).generate_from_frequencies(
+	cloud = WordCloud(background_color = "white", max_words=30, width=800, height=400).generate_from_frequencies(
 			x.T.sum(axis=1))
 	plt.figure(figsize=(20,10))
 	plt.imshow(cloud, interpolation="bilinear")
